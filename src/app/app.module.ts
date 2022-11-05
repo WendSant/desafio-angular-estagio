9167ecf904c62268490a6fe6821518b7ffbe6891
WendSant <wendsonsantanajps@gmail.com>
2022-11-05 17:03:35 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoticeCardComponent } from './components/notice-card/notice-card.component';
+import { IndexComponent } from './pages/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoticeCardComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/index/index.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css']
+})
+export class IndexComponent implements OnInit {
+
+  blabla= [1,2,3,4]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
